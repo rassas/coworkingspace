c54cd8ab5c11de103e8577ea4fea5e627fdf402d
Achref Rassas <rassasachref@gmail.com>
2020-03-16 17:01:05 +0100

    mailing prod config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV.fetch("SMTP_MAIL_FROM") { "mail@dev.com" }
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = ENV["DOMAINE"]
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_HOST"],
+    port: ENV["SMTP_PORT"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
